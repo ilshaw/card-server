a55a06d0993e2b9e7c763ba4b414c18d598921d0
ilshaw <pe4enka519@gmail.com>
2024-06-05 17:53:58 +0300

    Feature: added exception filter

--- src/core/controllers/auth.controller.ts
+++ src/core/controllers/auth.controller.ts
@@ -1,14 +1,16 @@
-import { UseInterceptors, Controller, UseGuards, Post, Request } from "@nestjs/common";
+import { UseInterceptors, UseFilters, Controller, UseGuards, Post, Request } from "@nestjs/common";
 import { CommandBus } from "@nestjs/cqrs";
 
 import { FastifyRequest } from "fastify";
 
 import { ClientResponseInterceptor } from "@core/interceptors/client-response.interceptor";
 import { PostAuthSignupCommand } from "@common/commands/post-auth-signup.command";
+import { ClientExceptionFilter } from "@core/filters/client-exception.filter";
 import { PostAuthSignupGuard } from "@core/guards/post-auth-signup.guard";
 import { PostAuthSignupDto } from "@common/dtos/post-auth-signup.dto";
 
 @UseInterceptors(ClientResponseInterceptor)
+@UseFilters(ClientExceptionFilter)
 @Controller("/auth")
 export class AuthController {
 	constructor(private readonly commandBus: CommandBus) {}
---
+++ src/core/filters/client-exception.filter.ts
@@ -0,0 +1,13 @@
+import { Catch, ExceptionFilter, ArgumentsHost } from "@nestjs/common";
+
+import { ClientExceptionClass } from "@common/classes/client-exception.class";
+
+@Catch(ClientExceptionClass)
+export class ClientExceptionFilter implements ExceptionFilter {
+	public catch(exception: ClientExceptionClass, host: ArgumentsHost) {
+		const response = exception.getResponse();
+		const status = exception.getStatus();
+	
+		return host.switchToHttp().getResponse().status(status).send(response);
+	}
+}
