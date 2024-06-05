47400a7d02bbbba1ab454cd60044c56e82d95251
ilshaw <pe4enka519@gmail.com>
2024-06-05 17:35:32 +0300

    Feature: added config validation

--- src/core/modules/config.module.ts
+++ src/core/modules/config.module.ts
@@ -1,12 +1,18 @@
 import { ConfigModule as NestConfigModule } from "@nestjs/config";
 import { Global, Module } from "@nestjs/common";
 
+import * as joi from "joi";
+
 import { ConfigService } from "@core/services/config.service";
 
 @Global()
 @Module({
 	imports: [
-    	NestConfigModule.forRoot()
+    	NestConfigModule.forRoot({
+			validationSchema: joi.object({
+				PORT: joi.number().required()
+			})
+		})
 	],
 	providers: [
 		ConfigService
