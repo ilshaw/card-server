ddf1fa57ec38653d42cf2497280095fefcbb5e70
ilshaw <pe4enka519@gmail.com>
2024-06-05 18:00:48 +0300

    Feature: added bcrypt module

--- src/core/commands/post-auth-signup.handler.ts
+++ src/core/commands/post-auth-signup.handler.ts
@@ -3,16 +3,20 @@ import { CommandHandler } from "@nestjs/cqrs";
 import { PostAuthSignupCommand } from "@common/commands/post-auth-signup.command";
 import { UserRepository } from "@core/repositories/user.repository";
 import { ResponseService } from "@core/services/response.service";
+import { BcryptService } from "@core/services/bcrypt.service";
 
 @CommandHandler(PostAuthSignupCommand)
 export class PostAuthSignupHandler {
 	constructor(
 		private readonly responseService: ResponseService,
-		private readonly userRepository: UserRepository
+		private readonly userRepository: UserRepository,
+		private readonly bcryptService: BcryptService
 	) {}
 
 	public async execute(command: PostAuthSignupCommand) {
-		const user = await this.userRepository.createByLoginAndPassword(command.request.body.login, command.request.body.password);
+		const hash = await this.bcryptService.hashData(command.request.body.password);
+
+		const user = await this.userRepository.createByLoginAndPassword(command.request.body.login, hash);
 
 		return this.responseService.createdResponse("User has successfully signed up", {
 			user: user
--- src/core/modules/app.module.ts
+++ src/core/modules/app.module.ts
@@ -5,6 +5,7 @@ import { ExceptionModule } from "@core/modules/exception.module";
 import { ResponseModule } from "@core/modules/response.module";
 import { StrategyModule } from "@core/modules/strategy.module";
 import { ConfigModule } from "@core/modules/config.module";
+import { BcryptModule } from "@core/modules/bcrypt.module";
 import { PrismaModule } from "@core/modules/prisma.module";
 import { AuthModule } from "@core/modules/auth.module";
 import { CqrsModule } from "@core/modules/cqrs.module";
@@ -16,6 +17,7 @@ import { CqrsModule } from "@core/modules/cqrs.module";
 		ResponseModule,
 		StrategyModule,
 		ConfigModule,
+		BcryptModule,
 		PrismaModule,
 		AuthModule,
 		CqrsModule
