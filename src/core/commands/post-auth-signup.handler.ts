52ebc52c20e1fe9a2d63ad43c792e07d458e12cd
ilshaw <pe4enka519@gmail.com>
2024-06-06 02:10:00 +0300

    Feature: updated jwt algorithm

--- src/core/commands/post-auth-signup.handler.ts
+++ src/core/commands/post-auth-signup.handler.ts
@@ -4,13 +4,15 @@ import { PostAuthSignupCommand } from "@common/commands/post-auth-signup.command
 import { UserRepository } from "@core/repositories/user.repository";
 import { ResponseService } from "@core/services/response.service";
 import { BcryptService } from "@core/services/bcrypt.service";
+import { JwtService } from "@core/services/jwt.service";
 
 @CommandHandler(PostAuthSignupCommand)
 export class PostAuthSignupHandler {
 	constructor(
 		private readonly responseService: ResponseService,
 		private readonly userRepository: UserRepository,
-		private readonly bcryptService: BcryptService
+		private readonly bcryptService: BcryptService,
+		private readonly jwtService: JwtService
 	) {}
 
 	public async execute(command: PostAuthSignupCommand) {
@@ -18,7 +20,14 @@ export class PostAuthSignupHandler {
 
 		const user = await this.userRepository.createByLoginAndPassword(command.request.body.login, hash);
 
+		const refresh = await this.jwtService.signRefresh({ id: user.id });
+		const access = await this.jwtService.signAccess({ id: user.id });
+
 		return this.responseService.createdResponse("User has successfully signed up", {
+			token: {
+				refresh: refresh,
+				access: access
+			},
 			user: user
 		});
 	}
