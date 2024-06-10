1d568808d53cdc67a0ec9e43e4ab2f38fb1a0ed6
ilshaw <pe4enka519@gmail.com>
2024-06-11 00:24:22 +0300

    Refactoring: updated get user profile strategy

--- src/core/strategies/get-user-profile.strategy.ts
+++ src/core/strategies/get-user-profile.strategy.ts
@@ -5,6 +5,7 @@ import { Strategy, ExtractJwt } from "passport-jwt";
 import { FastifyRequest } from "fastify";
 
 import { AccessPayloadInterface } from "@common/interfaces/access-payload.interface";
+import { SessionRepository } from "@core/repositories/session.repository";
 import { ExceptionService } from "@core/services/exception.service";
 import { UserRepository } from "@core/repositories/user.repository";
 import { KeyService } from "@core/services/key.service";
@@ -12,21 +13,30 @@ import { KeyService } from "@core/services/key.service";
 @Injectable()
 export class GetUserProfileStrategy extends PassportStrategy(Strategy, "get-user-profile") {
 	constructor(
+		private readonly sessionRepository: SessionRepository,
 		private readonly exceptionService: ExceptionService, 
 		private readonly userRepository: UserRepository,
 		private readonly keyService: KeyService
 	) {
 		super({
+			passReqToCallback: true,
 			jwtFromRequest: ExtractJwt.fromExtractors([(request: FastifyRequest) => request.cookies.access]),
 			secretOrKey: keyService.getPublic()
 		});
 	}
 
-	public async validate(payload: AccessPayloadInterface) {
+	public async validate(request: FastifyRequest, payload: AccessPayloadInterface) {
 		const user = await this.userRepository.findUniqueById(payload.id);
 
 		if(user) {
-			return user;
+			const session = await this.sessionRepository.findUniqueByUserAndAccess(user, request.cookies.access);
+
+			if(session) {
+				return user;
+			}
+			else {
+				throw this.exceptionService.notFoundException("Session was not found");
+			}
 		}
 		else {
 			throw this.exceptionService.notFoundException("User was not found");
