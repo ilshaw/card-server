3a6980c8b2b1b99a684f66457c58e5af0e10f55d
ilshaw <pe4enka519@gmail.com>
2024-06-10 04:00:28 +0300

    Refactoring: updated swagger documentation

--- src/common/dtos/post-auth-signup.dto.ts
+++ src/common/dtos/post-auth-signup.dto.ts
@@ -3,9 +3,9 @@ import { ApiProperty } from "@nestjs/swagger";
 import { UserEntity } from "@common/entities/user.entity";
 
 export class PostAuthSignupDto implements Pick<UserEntity, "password" | "login"> {
-	@ApiProperty({ type: String, required: true })
+	@ApiProperty({ type: String, required: true, example: "12345678", description: "Post auth signup password" })
 	public readonly password: string;
 
-	@ApiProperty({ type: String, required: true })
+	@ApiProperty({ type: String, required: true, example: "ilshaw", description: "Post auth signup login" })
 	public readonly login: string;
 }
--- src/core/controllers/auth.controller.ts
+++ src/core/controllers/auth.controller.ts
@@ -1,5 +1,5 @@
-import { UseInterceptors, UseFilters, Controller, UseGuards, Post, Request } from "@nestjs/common";
-import { ApiTags, ApiBody } from "@nestjs/swagger";
+import { UseInterceptors, UseFilters, Controller, UseGuards, HttpCode, Post, Request } from "@nestjs/common";
+import { ApiTags, ApiBody, ApiCreatedResponse, ApiConflictResponse } from "@nestjs/swagger";
 import { CommandBus } from "@nestjs/cqrs";
 
 import { FastifyRequest } from "fastify";
@@ -8,6 +8,7 @@ import { ClientResponseInterceptor } from "@core/interceptors/client-response.in
 import { PostAuthSignupCommand } from "@common/commands/post-auth-signup.command";
 import { ClientExceptionFilter } from "@core/filters/client-exception.filter";
 import { PostAuthSignupGuard } from "@core/guards/post-auth-signup.guard";
+import { ResponseStatusEnum } from "@common/enums/response-status.enum";
 import { PostAuthSignupDto } from "@common/dtos/post-auth-signup.dto";
 
 @ApiTags("Auth")
@@ -17,8 +18,11 @@ import { PostAuthSignupDto } from "@common/dtos/post-auth-signup.dto";
 export class AuthController {
 	constructor(private readonly commandBus: CommandBus) {}
 
-	@ApiBody({ type: PostAuthSignupDto, required: true })
+	@ApiConflictResponse({ description: "Login is already taken" })
+	@ApiCreatedResponse({ description: "User has successfully signed up" })
+	@ApiBody({ type: PostAuthSignupDto, required: true, description: "Post auth signup body" })
 	@UseGuards(PostAuthSignupGuard)
+	@HttpCode(ResponseStatusEnum.CREATED)
 	@Post("/signup")
 	public async postAuthSignup(@Request() request: FastifyRequest<{ Body: PostAuthSignupDto }>) {
 		return await this.commandBus.execute(new PostAuthSignupCommand(request));
