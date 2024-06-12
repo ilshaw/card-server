71d765266c935fd7f3bfd8dbea8b9ae804652d89
ilshaw <pe4enka519@gmail.com>
2024-06-12 13:56:53 +0300

    Refactoring: some updates

--- src/core/filters/server-exception.filter.ts
+++ src/core/filters/server-exception.filter.ts
@@ -3,14 +3,17 @@ import { Catch, ExceptionFilter, ArgumentsHost } from "@nestjs/common";
 import { FastifyReply } from "fastify";
 
 import { ServerExceptionClass } from "@common/classes/server-exception.class";
+import { PinoService } from "@core/services/pino.service";
 
 @Catch(ServerExceptionClass)
 export class ServerExceptionFilter implements ExceptionFilter {
+    constructor(private readonly pinoService: PinoService) {}
+
     public catch(exception: ServerExceptionClass, host: ArgumentsHost) {
         const response = exception.getResponse();
         const status = exception.getStatus();
 
-        console.log(response.cause);
+        this.pinoService.error(response.cause);
 	
         return host.switchToHttp().getResponse<FastifyReply>().status(status).send(response);
     }
