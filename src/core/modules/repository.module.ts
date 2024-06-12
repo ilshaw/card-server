15e792c14234f9e17684b3d998e1e41b6958f705
ilshaw <pe4enka519@gmail.com>
2024-06-12 15:04:16 +0300

    Refactoring: updated repository module

--- src/core/modules/repository.module.ts
+++ src/core/modules/repository.module.ts
@@ -1,15 +1,18 @@
 import { Global, Module } from "@nestjs/common";
 
+import { ConfirmationRepository } from "@core/repositories/confirmation.repository";
 import { SessionRepository } from "@core/repositories/session.repository";
 import { UserRepository } from "@core/repositories/user.repository";
 
 @Global()
 @Module({
     providers: [
+        ConfirmationRepository,
         SessionRepository,
         UserRepository
     ],
     exports: [
+        ConfirmationRepository,
         SessionRepository,
         UserRepository
     ]
