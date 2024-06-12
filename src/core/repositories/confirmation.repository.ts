586b0d67765a5126ccd09a4054f6b27f67eac326
ilshaw <pe4enka519@gmail.com>
2024-06-12 14:33:53 +0300

    Feature: added confirmation repository

---
+++ src/core/repositories/confirmation.repository.ts
@@ -0,0 +1,31 @@
+import { Injectable } from "@nestjs/common";
+
+import { PrismaService } from "@core/services/prisma.service";
+import { UserEntity } from "@common/entities/user.entity";
+
+@Injectable()
+export class SessionRepository {
+    constructor(private readonly prismaService: PrismaService) {}
+
+    public async createByUserAndConfirm(user: Omit<UserEntity, "password">, confirm: ConfirmTokenType) {
+        return await this.prismaService.confirmation.create({
+            data: {
+                user: {
+                    connect: user
+                },
+                confirm: confirm
+            }
+        });
+    }
+
+    public async findUniqueByUserAndConfirm(user: Omit<UserEntity, "password">, confirm: ConfirmTokenType) {
+        return await this.prismaService.confirmation.findUnique({
+            where: {
+                user_id_confirm: {
+                    user_id: user.id,
+                    confirm: confirm
+                }
+            }
+        });
+    }
+}
--- src/core/repositories/session.repository.ts
+++ src/core/repositories/session.repository.ts
@@ -7,7 +7,7 @@ import { UserEntity } from "@common/entities/user.entity";
 export class SessionRepository {
     constructor(private readonly prismaService: PrismaService) {}
 
-    public async createByUserAndAccessAndRefresh(user: Omit<UserEntity, "password">, access: string, refresh: string) {
+    public async createByUserAndAccessAndRefresh(user: Omit<UserEntity, "password">, access: AccessTokenType, refresh: RefreshTokenType) {
         return await this.prismaService.session.create({
             data: {
                 user: {
@@ -19,7 +19,7 @@ export class SessionRepository {
         });
     }
 
-    public async findUniqueByUserAndRefresh(user: Omit<UserEntity, "password">, refresh: string) {
+    public async findUniqueByUserAndRefresh(user: Omit<UserEntity, "password">, refresh: RefreshTokenType) {
         return await this.prismaService.session.findUnique({
             where: {
                 user_id_refresh: {
@@ -30,7 +30,7 @@ export class SessionRepository {
         });
     }
 
-    public async findUniqueByUserAndAccess(user: Omit<UserEntity, "password">, access: string) {
+    public async findUniqueByUserAndAccess(user: Omit<UserEntity, "password">, access: AccessTokenType) {
         return await this.prismaService.session.findUnique({ 
             where: {
                 user_id_access: {
